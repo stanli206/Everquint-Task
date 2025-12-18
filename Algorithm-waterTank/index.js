2e5cf37a18ba87665357374ce8146fcc2676066a
stantlin s <stantilin@gmail.com>
2025-12-18 15:29:49 +0530

    `Removed commented out code in Algorithm-waterTank/index.js`

--- Algorithm-waterTank/index.js
+++ Algorithm-waterTank/index.js
@@ -1,27 +1,27 @@
-function trapWater(height) {
-  let n = height.length;
-  let totalWater = 0;
+// function trapWater(height) {
+//   let n = height.length;
+//   let totalWater = 0;
 
-  for (let i = 0; i < n; i++) {
-    let leftMax = 0;
-    let rightMax = 0;
+//   for (let i = 0; i < n; i++) {
+//     let leftMax = 0;
+//     let rightMax = 0;
 
-    // left side max
-    for (let l = 0; l <= i; l++) {
-      leftMax = Math.max(leftMax, height[l]);
-    }
+//     // left side max
+//     for (let l = 0; l <= i; l++) {
+//       leftMax = Math.max(leftMax, height[l]);
+//     }
 
-    // right side max
-    for (let r = i; r < n; r++) {
-      rightMax = Math.max(rightMax, height[r]);
-    }
+//     // right side max
+//     for (let r = i; r < n; r++) {
+//       rightMax = Math.max(rightMax, height[r]);
+//     }
 
-    let waterAtI = Math.min(leftMax, rightMax) - height[i];
+//     let waterAtI = Math.min(leftMax, rightMax) - height[i];
 
-    if (waterAtI > 0) {
-      totalWater += waterAtI;
-    }
-  }
+//     if (waterAtI > 0) {
+//       totalWater += waterAtI;
+//     }
+//   }
 
-  return totalWater;
-}
+//   return totalWater;
+// }
